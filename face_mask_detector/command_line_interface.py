d1e2a6c5688abf47d409a4345eeafeaebb0fd269
Mark V. Senofsky <mark.v.senofsky@gmail.com>
2020-08-06 17:06:00 -0400

    Add command_line_interface.py which provides a CLI
    Users may still want to use face-mask-detector as a command line
    utility, therefore:
    - Add command_line_interface.py that will detect face masks in images

---
+++ face_mask_detector/command_line_interface.py
@@ -0,0 +1,59 @@
+"""This module provides an command line interface to face-mask-detector which
+   allows face-mask-detector to behave as a command line utility
+"""
+
+import argparse
+import sys
+
+from .file_helper import file_is_not_readable, directory_is_not_readable
+from .lib import display_image_with_face_mask_detections
+
+
+def _parse_args() -> argparse.Namespace:
+    """Parse the arguments given on the command line
+    """
+    arg_parser = argparse.ArgumentParser()
+
+    arg_parser.add_argument(
+        "--confidence",
+        "-c",
+        type=float,
+        default=0.5,
+        help="minimum probability to filter weak detections",
+    )
+    arg_parser.add_argument(
+        "--image", "-i", type=str, required=True, help="path to the input image"
+    )
+    arg_parser.add_argument(
+        "--verbose",
+        "-v",
+        action="count",
+        default=0,
+        help="enable verbose mode to print debug messages",
+    )
+
+    return arg_parser.parse_args()
+
+
+def _validate_args(args: argparse.Namespace) -> None:
+    """Raises an exception if any argument is invalid
+    """
+    if file_is_not_readable(args.image):
+        raise IOError(f"image is not readable: {args.image}")
+
+
+def main():
+
+    args = _parse_args()
+
+    try:
+        _validate_args(args)
+    except IOError as error:
+        print(error)
+        sys.exit(1)
+
+    display_image_with_face_mask_detections(args.image, args.confidence)
+
+
+if __name__ == "__main__":
+    main()
